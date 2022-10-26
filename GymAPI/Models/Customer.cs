8fb77b207938cbfb733d498d468b275da028ec5d
Jose Victor Persset <jpersset@gmail.com>
2022-10-26 15:52:10 -0300

    Add Entity Framework to project

--- GymAPI/Models/Customer.cs
+++ GymAPI/Models/Customer.cs
@@ -4,7 +4,7 @@
         public string Name { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public DateTime SubscriptionDeadline { get; set; }
-        public virtual ICollection<Routine> Routines { get; set;}
+        public virtual ICollection<Routine>? Routines { get; set;}
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
--- GymAPI/Models/Routine.cs
+++ GymAPI/Models/Routine.cs
@@ -1,7 +1,7 @@
 ﻿namespace GymAPI.Models {
     public class Routine {
         public int Id { get; set; }
-        public ICollection<Exercise> Exercises { get; set; }
+        public ICollection<Exercise>? Exercises { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
