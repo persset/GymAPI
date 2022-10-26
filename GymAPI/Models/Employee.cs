8b1d8affb1bdfbe0e3bea11295b5436a90e8c8f4
Jose Victor Persset <jpersset@gmail.com>
2022-10-26 15:11:55 -0300

    Create employee model

--- GymAPI/Models/Employee.cs
+++ GymAPI/Models/Employee.cs
@@ -1,4 +1,9 @@
 ﻿namespace GymAPI.Models {
     public class Employee {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
