038ed5f15702336e290bfafe1584fd3bb32034fd
Jose Victor Persset <jpersset@gmail.com>
2022-10-26 19:09:48 -0300

    Add exercise data context

---
+++ GymAPI/Data/ExerciseDataContext.cs
@@ -0,0 +1,13 @@
+﻿using GymAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAPI.Data
+{
+    public class ExerciseDataContext : DbContext
+    {
+        public ExerciseDataContext(DbContextOptions<ExerciseDataContext> options) : base(options) { }
+
+        public DbSet<Exercise>? Exercises { get; set; }
+    }
+
+}
