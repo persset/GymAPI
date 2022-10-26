ac953b23489478aeb86a7a9b89cfe907c23e8264
Jose Victor Persset <jpersset@gmail.com>
2022-10-26 19:01:59 -0300

    Create EmplyeeDataContext

---
+++ GymAPI/Data/EmployeeDataContext.cs
@@ -0,0 +1,14 @@
+﻿using GymAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAPI.Data
+{
+    public class EmployeeDataContext : DbContext
+    {
+        public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
