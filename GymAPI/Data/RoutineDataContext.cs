b43e6e321694c91882e39a15c218a9fb6aba0b4e
Jose Victor Persset <jpersset@gmail.com>
2022-10-26 19:11:37 -0300

    create routine data context

---
+++ GymAPI/Data/RoutineDataContext.cs
@@ -0,0 +1,12 @@
+﻿using GymAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAPI.Data
+{
+    public class RoutineDataContext : DbContext
+    {
+        public RoutineDataContext(DbContextOptions<RoutineDataContext> options : base(options) { 
+        }
+        public DbSet<Routine>? Routines { get; set; }
+    }
+}
