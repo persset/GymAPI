f5f487ab1055c8441e1c807c0617adb25ab9b454
Jose Victor Persset <jpersset@gmail.com>
2022-10-26 19:12:45 -0300

    Fix syntax error

--- GymAPI/Data/RoutineDataContext.cs
+++ GymAPI/Data/RoutineDataContext.cs
@@ -5,7 +5,7 @@ namespace GymAPI.Data
 {
     public class RoutineDataContext : DbContext
     {
-        public RoutineDataContext(DbContextOptions<RoutineDataContext> options : base(options) { 
+        public RoutineDataContext(DbContextOptions<RoutineDataContext> options) : base(options) { 
         }
         public DbSet<Routine>? Routines { get; set; }
     }
