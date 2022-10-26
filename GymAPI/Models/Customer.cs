da69dd7ef84a7f6a6d02ed2b520a0218813275ae
Jose Victor Persset <jpersset@gmail.com>
2022-10-26 15:30:02 -0300

    Create Customer model

---
+++ GymAPI/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace GymAPI.Models {
+    public class Customer {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime SubscriptionDeadline { get; set; }
+        public virtual ICollection<Routine> Routines { get; set;}
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
--- GymAPI/Models/Employee.cs
+++ GymAPI/Models/Employee.cs
@@ -5,5 +5,7 @@
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
