7eb555c9abd744423adfabe70a587106e482cc66
Jose Victor Persset <jpersset@gmail.com>
2023-08-02 14:45:17 -0300

    Update: Change to single datacontext usage

---
+++ GymAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using GymAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<Exercise> Exercises => Set<Exercise>();
+        public DbSet<Routine> Routines => Set<Routine>();
+
+    }
+}
