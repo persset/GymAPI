2a397d1803b92dff47498a91f6d4774348511044
Jose Victor Persset <jpersset@gmail.com>
2023-08-02 15:36:54 -0300

    Add: Setup initial empty migration

--- GymAPI/Data/DataContext.cs
+++ GymAPI/Data/DataContext.cs
@@ -1,4 +1,4 @@
-﻿using GymAPI.Models;
+﻿
 using Microsoft.EntityFrameworkCore;
 
 namespace GymAPI.Data
@@ -6,11 +6,5 @@ namespace GymAPI.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-
-        public DbSet<Customer> Customers => Set<Customer>();
-        public DbSet<Employee> Employees => Set<Employee>();
-        public DbSet<Exercise> Exercises => Set<Exercise>();
-        public DbSet<Routine> Routines => Set<Routine>();
-
     }
 }
