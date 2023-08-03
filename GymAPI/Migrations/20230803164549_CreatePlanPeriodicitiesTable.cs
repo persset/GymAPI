271ac645d8974a17fbde901ca0897c404320130e
Jose Victor Persset <jpersset@gmail.com>
2023-08-03 13:46:16 -0300

    Add: Create PlanPeriodiocities Table migration

---
+++ GymAPI/Migrations/20230803164549_CreatePlanPeriodicitiesTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymAPI.Migrations
+{
+    public partial class CreatePlanPeriodicitiesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlanPeriodicities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Period = table.Column<int>(type: "int", nullable: false),
+                    PeriodDiscount = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlanPeriodicities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlanPeriodicities");
+        }
+    }
+}
