5391d6252b502a5cd055d767e9023d609423e2da
Jose Victor Persset <jpersset@gmail.com>
2023-08-08 16:51:35 -0300

    Add: Generate Trainings table creation migration

---
+++ GymAPI/Migrations/20230808195116_CreateTrainingsTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymAPI.Migrations
+{
+    public partial class CreateTrainingsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trainings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    TrainingDesignerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trainings_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Trainings_Functionaries_TrainingDesignerId",
+                        column: x => x.TrainingDesignerId,
+                        principalTable: "Functionaries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trainings_ClientId",
+                table: "Trainings",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trainings_TrainingDesignerId",
+                table: "Trainings",
+                column: "TrainingDesignerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trainings");
+        }
+    }
+}
