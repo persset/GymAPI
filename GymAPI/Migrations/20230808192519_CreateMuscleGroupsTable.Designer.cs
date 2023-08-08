ea810eb0f819b34fb8e838409005335b3cf46e9b
Jose Victor Persset <jpersset@gmail.com>
2023-08-08 16:26:01 -0300

    Add: Generate MuscleGroups table migration

--- GymAPI/Migrations/20230808192519_CreateMuscleGroupsTable.Designer.cs
+++ GymAPI/Migrations/20230808192519_CreateMuscleGroupsTable.Designer.cs
@@ -3,6 +3,7 @@ using GymAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230808192519_CreateMuscleGroupsTable")]
+    partial class CreateMuscleGroupsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
